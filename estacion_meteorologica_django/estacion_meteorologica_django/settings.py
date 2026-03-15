6f2a88c0086c80cf8ff19d7811bd4a209b2f939b
rdmv1ingeniero <rdmv1ingeniero@gmail.com>
2026-03-15 16:02:01 +0000

    Modificacion del settings.py para que el sistema pueda ser accedido desde la url.

--- estacion_meteorologica_django/estacion_meteorologica_django/settings.py
+++ estacion_meteorologica_django/estacion_meteorologica_django/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h9&v1qmah5-5e*dj1()hd4i8d)=@+43a6x@lzux9kz$hei*qg%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://itt363-3.eict.ce.pucmm.edu.do/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'itt363-3.eict.ce.pucmm.edu.do']
 
-CSRF_TRUSTED_ORIGINS = ['https://itt363-X.eict.ce.pucmm.edu.do']
+CSRF_TRUSTED_ORIGINS = ['https://itt363-3.eict.ce.pucmm.edu.do']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
 USE_X_FORWARDED_PORT = True
@@ -81,8 +81,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'estacion_meteorologica',
         'USER': 'itt363',
-        'PASSWORD': 'd123d456',
-        'HOST': '192.168.100.153',
+        'PASSWORD': '9Xd%2@DpEdWH!M@',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
